271a4cce9778fec7fc67b4f69d5a670acb427f2a
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-02-17 17:08:44 +0100

    Esercizio base

---
+++ main.js
@@ -0,0 +1,20 @@
+const root = new Vue(
+    {
+        el: '#root',
+
+        data:{
+            emailArray: []
+        },
+            mounted(){
+                for(let i = 0; i <= 10; i++){
+                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((response) => {
+                    this.emailArray.push(response.data.response);
+                    });
+                    
+                }
+                
+            }
+
+    }    
+);
